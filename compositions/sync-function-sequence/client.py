c7ce61523a75a9b52d8310acf27be3e8b65a50fc
luusluus <lucienmartijn@gmail.com>
2021-10-13 15:43:14 +0200

    add gitiginore .aws-sam

--- compositions/sync-function-sequence/client.py
+++ compositions/sync-function-sequence/client.py
@@ -9,7 +9,7 @@ from compositions.helpers.api_gateway import get_api_gateway_endpoint
 endpoint = get_api_gateway_endpoint(api_get_way_name='sync-function-sequence')
 
 # call the first function a to start the workflow
-result = requests.get(endpoint + 'function/a')
+result = requests.get(endpoint + 'a')
 print(json.loads(result.content))
 
 
