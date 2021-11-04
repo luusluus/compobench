694b93b58694c8e6fcdf2fb863e022e885dfe191
luusluus <lucienmartijn@gmail.com>
2021-11-04 16:38:16 +0100

    update changes from master

--- compositions/sync-function-sequence/compose.py
+++ compositions/sync-function-sequence/compose.py
@@ -1,23 +1,13 @@
 import os
-import json
-
-from boto3 import client as boto3_client
-from botocore.config import Config
+from aws_lambda import LambdaHelper
 
 def compose(event, function_name, business_logic_function):
     result = business_logic_function(event)
     if function_name == '':
         return result
     else:
-        aws_region = os.environ['AWS_REGION']
-
-        client = boto3_client('lambda', region_name=aws_region)
-
-        print(f'Synchronously invoking {function_name}')
-        
-        response = client.invoke(
-            FunctionName=function_name,
-            InvocationType='RequestResponse',
-            Payload=json.dumps(result))
-
-        return json.load(response['Payload'])
+        lambda_helper = LambdaHelper(aws_region=os.environ['AWS_REGION'])
+        return lambda_helper.invoke_lambda(
+            function_name=function_name,
+            payload=result
+        )
