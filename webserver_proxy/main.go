37e0514ab4557a5a8c9cbcb771a75848edc090ef
luusluus <lucienmartijn@gmail.com>
2022-01-04 20:56:06 +0100

    increase concurrent workers

--- webserver_proxy/main.go
+++ webserver_proxy/main.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"strconv"
 
 	fs "webserver_proxy/function_sequence"
 )
@@ -25,11 +24,6 @@ func sequence(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 }
 
-func hello(w http.ResponseWriter, req *http.Request) {
-	w.WriteHeader(201)
-	w.Header().Set("Content-Type", "application/json")
-}
-
 func parse_message(req *http.Request) Message {
 	var m Message
 	err := json.NewDecoder(req.Body).Decode(&m)
@@ -39,19 +33,9 @@ func parse_message(req *http.Request) Message {
 	return m
 }
 
-func parse_python_response(response []byte) int {
-	out_string := string(response)
-	// remove new line
-	status_code, err := strconv.Atoi(out_string[:len(out_string)-1])
-	if err != nil {
-		fmt.Println(err)
-	}
-	return status_code
-}
-
 func main() {
 	http.HandleFunc("/sequence", sequence)
-	http.HandleFunc("/hello", hello)
+
 	fmt.Printf("Starting server at port 8000\n")
 	http.ListenAndServe(":8000", nil)
 }
