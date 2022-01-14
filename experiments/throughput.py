32bf132240e0e6abe0e20b4715e78a912cb02ac7
luusluus <lucienmartijn@gmail.com>
2022-01-14 22:04:24 +0100

    do workflow engine experiment

--- experiments/throughput.py
+++ experiments/throughput.py
@@ -76,9 +76,9 @@ workflow_engine_experiment_data = ThroughputExperimentData(
 # all_experiment_data.append(async_coordinator_experiment_data)
 # all_experiment_data.append(message_queue_experiment_data)
 # all_experiment_data.append(storage_based_experiment_data)
-all_experiment_data.append(event_sourcing_experiment_data)
-all_experiment_data.append(blackboard_experiment_data)
-# all_experiment_data.append(workflow_engine_experiment_data)
+# all_experiment_data.append(event_sourcing_experiment_data)
+# all_experiment_data.append(blackboard_experiment_data)
+all_experiment_data.append(workflow_engine_experiment_data)
 
 for experiment_data in all_experiment_data:
     experiment = ThroughputExperiment(experiment_data=experiment_data)
