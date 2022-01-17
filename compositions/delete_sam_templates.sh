a432f60bf36b02b87c5365c364a6f7fa1f97b4b8
luusluus <lucienmartijn@gmail.com>
2022-01-17 22:48:46 +0100

    update deploy and delete scripts

--- compositions/delete_sam_templates.sh
+++ compositions/delete_sam_templates.sh
@@ -6,7 +6,7 @@ cd ../compiled_sequence && sam delete --no-prompts && \
 cd ../client_side_scheduling && sam delete --no-prompts && \
 cd ../coordinator && sam delete --no-prompts && \
 cd ../event_sourcing && sam delete --no-prompts && \
-cd ../message_queue-based && sam delete --no-prompts && \
+cd ../message_queue_based && sam delete --no-prompts && \
 cd ../routing_slip && sam delete --no-prompts && \
 cd ../blackboard_based && sam delete --no-prompts && \
 cd ../storage_based && sam delete --no-prompts && \
--- compositions/deploy_sam_templates.sh
+++ compositions/deploy_sam_templates.sh
@@ -6,7 +6,7 @@ cd ../compiled_sequence && sam build --cached --beta-features && sam deploy --no
 cd ../client_side_scheduling && sam build --cached --beta-features && sam deploy --no-confirm-changeset && \
 cd ../coordinator && sam build --cached --beta-features && sam deploy --no-confirm-changeset && \
 cd ../event_sourcing && sam build --cached --beta-features && sam deploy --no-confirm-changeset && \
-cd ../message_queue-based && sam build --cached --beta-features && sam deploy --no-confirm-changeset && \
+cd ../message_queue_based && sam build --cached --beta-features && sam deploy --no-confirm-changeset && \
 cd ../routing_slip && sam build --cached --beta-features && sam deploy --no-confirm-changeset && \
 cd ../blackboard_based && sam build --cached --beta-features && sam deploy --no-confirm-changeset && \
 cd ../storage_based && sam build --cached --beta-features && sam deploy --no-confirm-changeset && \
